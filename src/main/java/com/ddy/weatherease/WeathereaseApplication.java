c1dbd07a5ce29715f9692abb4ecfb4eccbaecc39
Your Name <you@example.com>
2025-02-14 14:29:41 +0800

    创建项目

---
+++ src/main/java/com/ddy/weatherease/WeathereaseApplication.java
@@ -0,0 +1,13 @@
+package com.ddy.weatherease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathereaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeathereaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddy/weatherease/WeathereaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddy.weatherease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathereaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
